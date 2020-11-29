e91c9fbb7f8d06ffa1390c7b24e751c8f4667ef5
Mohammed Rizvi <morizvi07@gmail.com>
2020-11-28 22:18:20 -0500

    worked on props and parent-child relationship and vice versa

---
+++ cmp-communication-01-starting-setup/src/components/NewFriend.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label for="name">Name</label>
+      <input type="text" id="name" v-model="enteredName" />
+    </div>
+    <div>
+      <label for="phone">Phone</label>
+      <input type="tel" id="phone" v-model="enteredPhone" />
+    </div>
+    <div>
+      <label for="email">E-Mail</label>
+      <input type="email" id="email" v-model="enteredEmail" />
+    </div>
+    <div>
+      <button>Add Friend</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["add-contact"],
+  data() {
+    return {
+      enteredName: "",
+      enteredPhone: "",
+      enteredEmail: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit(
+        "add-contact",
+        this.enteredName,
+        this.enteredPhone,
+        this.enteredEmail
+      );
+    },
+  },
+};
+</script>
