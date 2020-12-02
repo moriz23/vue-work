671f4f3150c0cd9f1982295403109b920e3b057a
Mohammed Rizvi <morizvi07@gmail.com>
2020-12-02 16:16:55 -0500

    Worked with animations in vue

---
+++ vue-animations/src/pages/AllUsers.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <h2>All Users</h2>
+    <router-link to="/courses">Course Goals</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ vue-animations/src/pages/CourseGoals.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <h2>All Courses</h2>
+    <router-link to="/">All Users</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
