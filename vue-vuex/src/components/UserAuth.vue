e111969b1a2188a597f078854ba8530f484aeb2a
Mohammed Rizvi <morizvi07@gmail.com>
2020-12-03 20:21:17 -0500

    Worked with vuex

---
+++ vue-vuex/src/components/UserAuth.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="login" v-if="!isAuth">Login</button>
+  <button @click="logout" v-else>Logout</button>
+  <p>{{ isTestAuth }}</p>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  methods: {
+    ...mapActions({
+      login: 'login',
+      logout: 'logout'
+    })
+  },
+  computed: {
+    isAuth() {
+      return this.$store.getters.userIsAuthenticated;
+    },
+    isTestAuth() {
+      return this.$store.getters.testAuth;
+    }
+  }
+};
+</script>
